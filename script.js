6c4400b782d1a01701838c3019c69a54bc8b7a08
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-18 19:38:08 -0500

    still looking for a solution to localStorage.getItem to return items in the correct place

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ USE CASES:
 
 // current day
 var today = $('#currentDay');
+// set text of today to the current day and time
 today.text(moment().format("[Today is] dddd[!] [the time is] h[:]mma"));
+var currentHour = moment().format('ha')
+
 
 // timeblocks
 var timeblock;
@@ -30,10 +33,8 @@ var storedTodos = JSON.parse(localStorage.getItem('Todo')) || [];
 // current text of text area to be saved to local storage
 var todo;
 
-
-
 // FUNCTIONS
-    // function to populate the time blocks
+// function to populate the time blocks
 function timeblockPopulator() {
     // for each hour between 9 and 5,
     for (let i = 0; i < timeblockTime.length; i++) {
@@ -55,103 +56,96 @@ function timeblockPopulator() {
         timeblock.append(saveBtn);
     };
     // set id for each textarea to time-[i]
-        $('textarea').each(function(i) {
-            $(this).attr('id', 'time-' + [i]);
-        });
+    $('textarea').each(function (i) {
+        $(this).attr('id', 'time-' + [i]);
+    });
 };
+
 timeblockPopulator();
 
+// color change of timeblock depending on time of day
+// for each block...
+// default time to future.
+// if time is present, remove future class
+// add present class
+if (currentHour == '9am') {
+    $('#time-0').removeClass("future").addClass("present");
+} if (currentHour == '10am') {
+    $('#time-1').removeClass("future").addClass("present");
+} if (currentHour == '11am') {
+    $('#time-2').removeClass("future").addClass("present");
+} if (currentHour == '12pm') {
+    $('#time-3').removeClass("future").addClass("present");
+} if (currentHour == '1pm') {
+    $('#time-4').removeClass("future").addClass("present");
+} if (currentHour == '2pm') {
+    $('#time-5').removeClass("future").addClass("present");
+} if (currentHour == '3pm') {
+    $('#time-6').removeClass("future").addClass("present");
+} if (currentHour == '4pm') {
+    $('#time-7').removeClass("future").addClass("present");
+} if (currentHour == '5pm') {
+    $('#time-8').removeClass("future").addClass("present");
+};
+
+// if time is past, add remove present class
+// add past class
+if (currentHour > '9am' || currentHour < '9am') {
+    $('#time-0').removeClass("future").addClass("past");
+} if (currentHour > '10am' || currentHour < '10am') {
+    $('#time-1').removeClass("future").addClass("past");
+} if (currentHour > '11am' || currentHour < '11am') {
+    $('#time-2').removeClass("future").addClass("past");
+} if (currentHour > '12pm' || currentHour < '12pm') {
+    $('#time-3').removeClass("future").addClass("past");
+} if (currentHour > '1pm' || currentHour < '1pm') {
+    $('#time-4').removeClass("future").addClass("past");
+} if (currentHour > '2pm' || currentHour < '2pm') {
+    $('#time-5').removeClass("future").addClass("past");
+} if (currentHour > '3pm' || currentHour < '3pm') {
+    $('#time-6').removeClass("future").addClass("past");
+} if (currentHour > '4pm' || currentHour < '4pm') {
+    $('#time-7').removeClass("future").addClass("past");
+} if (currentHour > '5pm' || currentHour < '5pm') {
+    $('#time-8').removeClass("future").addClass("past");
+};
 
 // EVENT LISTENERS
 // Save button on click
-$(".saveBtn").on('click', function() {
+$(".saveBtn").on('click', function () {
     // get the text of the box next to the save button clicked
-        // select the textarea closest to the save button clicked
+    // select the textarea closest to the save button clicked
     var todo = $(this).prev('textarea').val();
     // set up our localstorage object
     var object = {
         todo: todo
-    }; 
+    };
     // push object into storedTodos array
     storedTodos.push(object)
     // when the button is pressed, save the data from the input box/form to local storage
     localStorage.setItem('Todo', JSON.stringify(storedTodos));
 });
 
-// on page load
-// $(document).ready(function() {
-//     $('textarea').text(storedTodos);
+// stack overflow solution (can't get it to work here)
+// $('textarea[type="text"]').each(function(){    
+//     var id = $(this).attr('id');
+//     var value = $(this).val();
+//    localStorage.setItem(id, JSON.stringify(value));
+
+// });   
+
+// $('textarea[type="textarea"]').each(function(){    
+//     var id = $(this).attr('id');
+//     var value = JSON.parse(localStorage.getItem(id));
+
+//     // $(this).val(value);
+
 // });
-    // set text of textarea to the locally stored todo
 
 
-// color change of timeblock depending on time of day
-    // for each block...
-if (moment().format('ha') == '9am') {
-    $('#time-0').removeClass("future").addClass("present");
-} if (moment().format('ha') == '10am') {
-    $('#time-1').removeClass("future").addClass("present");
-} if (moment().format('ha') == '11am') {
-    $('#time-2').removeClass("future").addClass("present");
-} if (moment().format('ha') == '12pm') {
-    $('#time-3').removeClass("future").addClass("present");
-} if (moment().format('ha') == '1pm') {
-    $('#time-4').removeClass("future").addClass("present");
-} if (moment().format('ha') == '2pm') {
-    $('#time-5').removeClass("future").addClass("present");
-} if (moment().format('ha') == '3pm') {
-    $('#time-6').removeClass("future").addClass("present");
-} if (moment().format('ha') == '4pm') {
-    $('#time-7').removeClass("future").addClass("present");
-} if (moment().format('ha') == '5pm') {
-    $('#time-8').removeClass("future").addClass("present");
-};
+// on page load
+// set text of textarea to the locally stored todo
+$(document).ready(function () {
+  
 
-if (moment().format('ha') < '9am') {
-
-    $('#time-0').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '10am') {
-
-    $('#time-1').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '11am') {
-
-    $('#time-2').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '12pm') {
-
-    $('#time-3').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '1pm') {
-
-    $('#time-4').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '2pm') {
-
-    $('#time-5').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '3pm') {
-
-    $('#time-6').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '4pm') {
-
-    $('#time-7').removeClass("future").addClass("past");
-
-} if (moment().format('ha') > '5pm') {
-
-    $('#time-8').removeClass("future").addClass("past");
-
-};
-
-
-    // default time to future.
-    // if time is present, add remove future class
-        // add present class
-    // if time is past, add remove present class
-        // add past class
-
-
-
-// ????
+});
