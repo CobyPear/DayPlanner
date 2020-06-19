adfdf292f38512489186894acc1c8b2698c2c9be
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-19 17:21:16 -0500

    app works properly and is properly formatted now. also updated README

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Day Planner
 
 This application helps you plan your 9a-5p workflow. The application can be found here: https://cobypear.github.io/DayPlanner/ 
+
+As you can see in the GIF below, the scheduler displays the day and time.
+The hour blocks are color coded depending on if the time is past (grey), future (green), or present (red).
+If you click the save button on the block you've entered text in, the text will persist through reloading the page.
+May you find productivity in your workday.
+
+![Work Day Scheduler GIF](https://i.imgur.com/8GYrjof.gif)
--- script.js
+++ script.js
@@ -18,134 +18,71 @@ USE CASES:
 var today = $('#currentDay');
 // set text of today to the current day and time
 today.text(moment().format("[Today is] dddd[!] [the time is] h[:]mma"));
-var currentHour = moment().format('ha')
 
+var storedTodos = JSON.parse(localStorage.getItem('Todo')) || {};
 
-// timeblocks
-var timeblock;
-var timeblockTime = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-// text form
-var todoForm;
-// save button
-var saveBtn = $(".saveBtn");
-// local storage
-var storedTodos = JSON.parse(localStorage.getItem('Todo')) || [];
-// current text of text area to be saved to local storage
-var todo;
+function buildRow(hour) {
+    var NOW = moment(hour, "H")
+    var baseClass = "future";
+    var action = storedTodos[hour]
 
-// FUNCTIONS
+    if (moment().isSame(NOW, "hour")) {
+        baseClass = "present"
+    }
+    else if (moment().isAfter(NOW)) {
+        baseClass = "past"
+    }
+
+    // timeblock now holds a new div
+    var timeblock = $("<div>");
+    // add appropriate classes to timeblock
+    timeblock.addClass('time-block row');
+    timeblock.attr("data-hour", hour);
+    // add appropriate text to timeblock
+
+    var hourEl = $("<div>")
+    hourEl.addClass('hour col-2')
+    hourEl.text(NOW.format("hA"));
+
+    // create an input form for each timeblock
+    var todoForm = $("<textarea>");
+    todoForm.addClass('description col-8 ' + baseClass);
+    todoForm.val(action)
+
+    // save button 
+    var saveBtn = $("<button>").addClass('saveBtn col-2');
+    var icon = $("<i>")
+    icon.addClass("fas fa-save")
+    saveBtn.append(icon)
+
+    timeblock.append(hourEl, todoForm, saveBtn)
+
+    // append timeblock to the container div
+    $('.container').append(timeblock);
+}
 // function to populate the time blocks
 function timeblockPopulator() {
     // for each hour between 9 and 5,
-    for (let i = 0; i < timeblockTime.length; i++) {
-        // timeblock now holds a new div
-        timeblock = $("<timeblock>");
-        // add appropriate classes to timeblock
-        timeblock.addClass('time-block row hour description');
-        // add appropriate text to timeblock
-        timeblock.text(timeblockTime[i]);
-        // create an input form for each timeblock
-        todoForm = $("<textarea>").addClass("future textarea").attr('type', 'text textarea');
-        // save button 
-        saveBtn = $("<button>").addClass('saveBtn i');
-        // append timeblock to the container div
-        $('.container').append(timeblock);
-        // add the form to the timeblock
-        timeblock.append(todoForm);
-        // add save button
-        timeblock.append(saveBtn);
+    for (let i = 9; i <= 17; i++) {
+        buildRow(i)
     };
-    // set id for each textarea to time-[i]
-    $('textarea').each(function (i) {
-        $(this).attr('id', 'time-' + [i]);
-    });
 };
 
 timeblockPopulator();
 
-// color change of timeblock depending on time of day
-// for each block...
-// default time to future.
-// if time is present, remove future class
-// add present class
-if (currentHour == '9am') {
-    $('#time-0').removeClass("future").addClass("present");
-} if (currentHour == '10am') {
-    $('#time-1').removeClass("future").addClass("present");
-} if (currentHour == '11am') {
-    $('#time-2').removeClass("future").addClass("present");
-} if (currentHour == '12pm') {
-    $('#time-3').removeClass("future").addClass("present");
-} if (currentHour == '1pm') {
-    $('#time-4').removeClass("future").addClass("present");
-} if (currentHour == '2pm') {
-    $('#time-5').removeClass("future").addClass("present");
-} if (currentHour == '3pm') {
-    $('#time-6').removeClass("future").addClass("present");
-} if (currentHour == '4pm') {
-    $('#time-7').removeClass("future").addClass("present");
-} if (currentHour == '5pm') {
-    $('#time-8').removeClass("future").addClass("present");
-};
-
-// if time is past, add remove present class
-// add past class
-if (currentHour > '9am' || currentHour < '9am') {
-    $('#time-0').removeClass("future").addClass("past");
-} if (currentHour > '10am' || currentHour < '10am') {
-    $('#time-1').removeClass("future").addClass("past");
-} if (currentHour > '11am' || currentHour < '11am') {
-    $('#time-2').removeClass("future").addClass("past");
-} if (currentHour > '12pm' || currentHour < '12pm') {
-    $('#time-3').removeClass("future").addClass("past");
-} if (currentHour > '1pm' || currentHour < '1pm') {
-    $('#time-4').removeClass("future").addClass("past");
-} if (currentHour > '2pm' || currentHour < '2pm') {
-    $('#time-5').removeClass("future").addClass("past");
-} if (currentHour > '3pm' || currentHour < '3pm') {
-    $('#time-6').removeClass("future").addClass("past");
-} if (currentHour > '4pm' || currentHour < '4pm') {
-    $('#time-7').removeClass("future").addClass("past");
-} if (currentHour > '5pm' || currentHour < '5pm') {
-    $('#time-8').removeClass("future").addClass("past");
-};
-
 // EVENT LISTENERS
 // Save button on click
+ // when the button is pressed, save the data from the input box/form to local storage
 $(".saveBtn").on('click', function () {
-    // get the text of the box next to the save button clicked
-    // select the textarea closest to the save button clicked
-    var todo = $(this).prev('textarea').val();
-    // set up our localstorage object
-    var object = {
-        todo: todo
-    };
-    // push object into storedTodos array
-    storedTodos.push(object)
-    // when the button is pressed, save the data from the input box/form to local storage
+    
+// select the textarea closest to the save button clicked
+// set up our localstorage object
+// get the text of the box next to the save button clicked
+    var todo = $(this).parent();
+    var hour = todo.attr('data-hour');
+    var action = todo.find('textarea').val();
+
+    storedTodos[hour] = action;
+
     localStorage.setItem('Todo', JSON.stringify(storedTodos));
-});
-
-// stack overflow solution (can't get it to work here)
-// $('textarea[type="text"]').each(function(){    
-//     var id = $(this).attr('id');
-//     var value = $(this).val();
-//    localStorage.setItem(id, JSON.stringify(value));
-
-// });   
-
-// $('textarea[type="textarea"]').each(function(){    
-//     var id = $(this).attr('id');
-//     var value = JSON.parse(localStorage.getItem(id));
-
-//     // $(this).val(value);
-
-// });
-
-
-// on page load
-// set text of textarea to the locally stored todo
-$(document).ready(function () {
-  
-
 });
